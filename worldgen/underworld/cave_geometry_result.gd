373611674dd2dc115a38974c6c478251e3ea59ae
KevinRabbe <rabbekevin@icloud.com>
2026-08-28 01:12:12 +0200

    Index cave descriptions by bounded 3D geometry cells

--- worldgen/underworld/cave_geometry_result.gd
+++ worldgen/underworld/cave_geometry_result.gd
@@ -1,10 +1,15 @@
 extends RefCounted
 class_name UnderworldCaveGeometryResult
 
+const CellPartitioner := preload("res://worldgen/geometry/geometry_cell_partitioner.gd")
+const CanonicalValue := preload("res://worldgen/validation/canonical_value.gd")
+
 var bundle
 var chamber_descriptors: Array
 var tunnel_descriptors: Array
 var reserved_site_descriptors: Array
+var cell_contributions: Array
+var cell_partition_fingerprint: String
 var geometry_metrics: Dictionary
 var fingerprint: String
 
@@ -22,5 +27,15 @@ func _init(
 	reserved_site_descriptors = []
 	if bundle_value != null:
 		reserved_site_descriptors = bundle_value.special_location_hooks.duplicate()
+	cell_contributions = CellPartitioner.build(
+		bundle_value,
+		chamber_descriptors,
+		tunnel_descriptors,
+		reserved_site_descriptors
+	)
+	var cell_data: Array = []
+	for contribution in cell_contributions:
+		cell_data.append(contribution.canonical_data())
+	cell_partition_fingerprint = "geometry-cells-" + CanonicalValue.fingerprint(cell_data)
 	geometry_metrics = metrics_value.duplicate(true)
 	fingerprint = fingerprint_value
