12b5d2754b612ed0e560484880a912d51daf92af
KevinRabbe <rabbekevin@icloud.com>
2026-08-28 01:06:08 +0200

    Expose reserved special-site volumes to geometry consumers

--- worldgen/underworld/cave_geometry_result.gd
+++ worldgen/underworld/cave_geometry_result.gd
@@ -4,6 +4,7 @@ class_name UnderworldCaveGeometryResult
 var bundle
 var chamber_descriptors: Array
 var tunnel_descriptors: Array
+var reserved_site_descriptors: Array
 var geometry_metrics: Dictionary
 var fingerprint: String
 
@@ -18,5 +19,9 @@ func _init(
 	bundle = bundle_value
 	chamber_descriptors = chamber_descriptors_value.duplicate()
 	tunnel_descriptors = tunnel_descriptors_value.duplicate()
+	reserved_site_descriptors = []
+	if bundle_value != null:
+		reserved_site_descriptors = bundle_value.special_location_hooks.duplicate()
 	geometry_metrics = metrics_value.duplicate(true)
+	geometry_metrics["reserved_site_count"] = reserved_site_descriptors.size()
 	fingerprint = fingerprint_value
