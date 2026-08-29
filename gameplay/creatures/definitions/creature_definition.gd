d952edb6b0ad2a5c2eef547ada2ef3daa6c112c7
KevinRabbe <rabbekevin@icloud.com>
2026-08-29 14:16:57 +0200

    ENEMY-001: fix inherited schema helper shadowing

--- gameplay/creatures/definitions/creature_definition.gd
+++ gameplay/creatures/definitions/creature_definition.gd
@@ -1,8 +1,6 @@
 extends "res://core/content/registry/content_definition.gd"
 
-const ContentId := preload("res://core/content/identity/content_id.gd")
 const ContentReference := preload("res://core/content/references/content_reference.gd")
-const SchemaId := preload("res://core/content/schema/schema_id.gd")
 
 const CREATURE_FAMILY := "creature"
 const ATTACK_PROFILE_FAMILY := "attack_profile"
