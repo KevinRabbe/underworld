0a45bc566f4bb48c246879786733c2032ac77497
KevinRabbe <rabbekevin@icloud.com>
2026-08-29 14:17:19 +0200

    ENEMY-001: fix authored Burrower startup type guard

--- gameplay/creatures/spawning/prototype_burrower_encounter_controller.gd
+++ gameplay/creatures/spawning/prototype_burrower_encounter_controller.gd
@@ -81,7 +81,7 @@ func _spawn_enemy_near_player() -> void:
 
 
 func _validate_burrower_definition() -> bool:
-	if BurrowerDefinition == null or not BurrowerDefinition is CreatureDefinition:
+	if BurrowerDefinition == null or BurrowerDefinition.get_script() != CreatureDefinition:
 		push_error("Burrower authored creature definition did not load as CreatureDefinition")
 		return false
 	var failures: Array[String] = BurrowerDefinition.validate_definition()
